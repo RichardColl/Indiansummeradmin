8f9efade2a4f5e4dbd29a037111d70d898291d0c
Richard Coll <richarddcoll@gmail.com>
2022-06-13 19:15:11 +0100

    import the http stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
@@ -18,6 +19,7 @@ import { LoginService } from './shared/login/login.service';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
      MatButtonModule,
         MatMenuModule,
         MatIconModule,
