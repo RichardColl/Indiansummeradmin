0b5d490c1244f0f3f4d9cc7bea415e15580bfc20
Richard Coll <richarddcoll@gmail.com>
2022-06-13 16:56:12 +0100

    adding the login code

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginService } from '../shared/login/login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form;
+    constructor(private fb: FormBuilder,
+      private myRoute: Router,
+      private loginserv: LoginService ) {
+      this.form = fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required]
+      });
+    }
 
   ngOnInit() {
   }
 
-}
+  login() {
+
+            if (this.form.valid) {
+                 // this.loading = true;
+
+                  this.loginserv.validate(this.form.value.email, this.form.value.password)
+                      .subscribe(
+                            (response: any) => {
+                             // callback(response.status);
+                              //localStorage.setItem('token', response.token);
+                              if(response.validlogin == 1) {
+
+
+                                this.myRoute.navigate(["indiansummeradmin"]);
+                              }
+                              else{
+                                alert("not valid login");
+                              }
+                            });
+
+                 }
+              }
+
+
+  }
--- src/app/shared/login/login.service.ts
+++ src/app/shared/login/login.service.ts
@@ -1,9 +1,50 @@
+//Richard Coll
+//23-8-2018
+
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json; charset=utf-8'
+
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  public API = '//indiansummerrecordsserver.herokuapp.com';
+
+
+  constructor(private http: HttpClient) { }
+
+
+  validate(username: string, password: string) {
+
+    httpOptions.headers.append('Access-Control-Allow-Origin', 'https://indiansummer.herokuapp.com');
+    httpOptions.headers.append('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    httpOptions.headers.append('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    httpOptions.headers.append('Access-Control-Allow-Credentials', 'true');
+
+    return this.http.get(this.API + '/validatelogin/?username=' + username + '&' + 'passw=' + password, httpOptions );
+       //return this.http.get(this.API + '/tendo-items');
+
+       //below works for username alone
+       //return this.http.post(this.API + '/login?xxxxxusername=' + username , username);
+         // return this.http.post<any>(this.API + '/login', { username: username, password: password })
+           //   .map(user => {
+                  // login successful if there's a jwt token in the response
+           //       if (user && user.token) {
+                      // store user details and jwt token in local storage to keep user logged in between page refreshes
+           //           localStorage.setItem('currentUser', JSON.stringify(user));
+           //       }
+
+           //       return user;
+           //   });
+      }
 }
