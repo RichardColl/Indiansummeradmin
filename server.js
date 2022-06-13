3d4bd7d819122246cb30e6722fd7e6eceb658bab
Richard Coll <richarddcoll@gmail.com>
2022-06-13 15:24:25 +0100

    added new server.js

---
+++ server.js
@@ -0,0 +1,43 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+//var mongodb = require("mongodb");
+//var ObjectID = mongodb.ObjectID;
+
+var CONTACTS_COLLECTION = "contacts";
+
+var app = express();
+app.use(bodyParser.json());
+
+// Create link to Angular build directory
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+
+// Connect to the database before starting the application server.
+//mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
+//  if (err) {
+//    console.log(err);
+//    process.exit(1);
+//  }
+
+  // Save database object from the callback for reuse.
+//  db = client.db();
+//  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+
+
