695e418ce02407d258ff9740f23b3ef45b1bd14f
Richard Coll <richarddcoll@gmail.com>
2024-02-28 18:37:56 +0000

    1 service unit test added it works

--- src/app/shared/artist/artist.service.spec.ts
+++ src/app/shared/artist/artist.service.spec.ts
@@ -1,9 +1,28 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ArtistService } from './artist.service';
+import { HttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { async, ComponentFixture } from '@angular/core/testing';
 
-describe('ArtistService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+fdescribe('ArtistService', () => {
+   beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        imports: [
+
+              HttpClientModule
+        ],
+        providers: [
+
+             ArtistService,
+            HttpClient
+
+
+         ]
+
+      })
+
+    }));
 
   it('should be created', () => {
     const service: ArtistService = TestBed.get(ArtistService);
