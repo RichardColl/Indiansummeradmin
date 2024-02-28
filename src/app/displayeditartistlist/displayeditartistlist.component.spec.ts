8effcc47a6c00beeac3821a802bb6fa81fa958fd
Richard Coll <richarddcoll@gmail.com>
2024-02-28 17:51:53 +0000

    1 test added to display artist list doesn't work

--- src/app/displayeditartistlist/displayeditartistlist.component.spec.ts
+++ src/app/displayeditartistlist/displayeditartistlist.component.spec.ts
@@ -31,11 +31,19 @@ const overrideArtistlistprovider = artlistrespData => {
     })
 }
 
-describe('DisplayeditartistlistComponent', () => {
+const theartist = {
+                    id: 123,
+                    name: 'abc',
+                    menuimage: 'def',
+                    history: 'ghi'
+           };
+
+fdescribe('DisplayeditartistlistComponent', () => {
   let component: DisplayeditartistlistComponent;
   let fixture: ComponentFixture<DisplayeditartistlistComponent>;
   let tmpArtistSrv;
   let debugElement: DebugElement;
+  let mockArtistService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -49,7 +57,7 @@ describe('DisplayeditartistlistComponent', () => {
       providers: [
           {
            provide:ArtistService,
-           useClass: ArtistService
+           useClass: mockArtistService
            },
            {
               provide:HttpClient,
@@ -62,55 +70,9 @@ describe('DisplayeditartistlistComponent', () => {
 
   }));
 
-describe('verify we create component', () => {
-  it('should create DisplayeditartistlistComponent', () => {
-
-        fixture = TestBed.createComponent(DisplayeditartistlistComponent);
-                          component = fixture.componentInstance;
-                          debugElement = fixture.debugElement;
-                          fixture.detectChanges();
-      const theartist = {
-          id: 123,
-          name: 'abc',
-          menuimage: 'def',
-          history: 'ghi'
-      }
-
-      //fix this later
-      //this would probably need to be an array of artist objects rather that one artist const
-      //the _embedded structure probably complicates things ???
-      overrideArtistlistprovider(theartist);
-
-   expect(component).toBeTruthy();
 
 
-  });
-
-  it('verify init service call', () => {
-
-        fixture = TestBed.createComponent(DisplayeditartistlistComponent);
-                          component = fixture.componentInstance;
-                          debugElement = fixture.debugElement;
-                          fixture.detectChanges();
-         tmpArtistSrv = debugElement.injector.get(ArtistService);
-
-         spyOn(tmpArtistSrv, 'getArtistsByDisplay').and.returnValue({});
-         component.ngOnInit();
-
-         expect(tmpArtistSrv.getArtistsByDisplay).toHaveBeenCalledTimes(1);
-
-
-
-    });
-
-     afterEach(() => {
-            fixture.destroy();
-          });
-
-  });
-
-
-  describe('calculate the state from data', () => {
+describe('calculate the state from data', () => {
 
         it('verify init service call', () => {
             //we mock up the artistservice observable
@@ -120,19 +82,20 @@ describe('verify we create component', () => {
                     component = fixture.componentInstance;
                     debugElement = fixture.debugElement;
                     fixture.detectChanges();
-            TestBed.overrideProvider(ArtistService, {
-                    useValue: {
-                        serviceData$: of({
-                            artistServiceState: ServiceState.IN_PROGRESS,
-                            artistDetails: {}
 
-                        })
-                    }
+             mockArtistService.serviceData$.next({
+                    state:ServiceState.SUCCESS,
+                    data:theartist
              });
 
             //access the components observable in progress state
+            //the test isn't working maybe async issues
+            //plus thedata won't exactly match theartist model
+
             component.displayArtistsOptionsState$.subscribe(viewState => {
-                expect(viewState).toBe(ServiceState.IN_PROGRESS);
+                fixture.detectChanges();
+                expect(viewState).toBe(ServiceState.SUCCESS);
+                expect(component.thedata).toEqual(theartist);
             });
 
 
@@ -141,42 +104,6 @@ describe('verify we create component', () => {
      });
 
   });
-
-//this last test won't work error thrown in afterAll
-//need to fix later
-//problem is when and where to create the fixture and component
-//and make them withing async or sync calls ????
-describe('calculate the state from data', () => {
-   it('verify init service call', () => {
-          //we mock up the artistservice observable
-          //and give it an in progress state
-
-          fixture = TestBed.createComponent(DisplayeditartistlistComponent);
-                  component = fixture.componentInstance;
-                  debugElement = fixture.debugElement;
-                  fixture.detectChanges();
-
-          const theartist = {
-                    id: 123,
-                    name: 'abc',
-                    menuimage: 'def',
-                    history: 'ghi'
-           }
-
-           //fix this later
-           //this would probably need to be an array of artist objects rather that one artist const
-           //the _embedded structure probably complicates things ???
-           overrideArtistlistprovider(theartist);
-
-          //access the components observable in progress state
-          component.displayArtistsOptionsState$.subscribe(viewState => {
-              expect(viewState).toBe(ServiceState.SUCCESS);
-          });
-
-
-
-   });
-});
 
 
 });
