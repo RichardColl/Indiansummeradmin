829e4f3a199569894aad8981ecc8bed2be3ed5bd
Richard Coll <richarddcoll@gmail.com>
2024-03-12 19:31:28 +0000

    adding artist summary

--- src/app/artistlistsummary/artistlistsummary.component.ts
+++ src/app/artistlistsummary/artistlistsummary.component.ts
@@ -3,6 +3,8 @@ import { Observable , BehaviorSubject} from 'rxjs';
 import { ArtistServiceData } from '../shared/artist.abstract.service';
 
 import { AdminListener, AdminType } from '../shared/utils/listener.service';
+import { ServiceState } from '../shared/main-api.service';
+import { pluck } from 'rxjs/operators';
 
 @Component({
   selector: 'app-artistlistsummary',
@@ -11,12 +13,21 @@ import { AdminListener, AdminType } from '../shared/utils/listener.service';
 })
 export class ArtistlistsummaryComponent implements OnInit {
 
+  @Input()
+  data$: Observable<ArtistServiceData>;
+
+  public readonly ServiceState = ServiceState;
+
+  public viewState$: Observable<ServiceState>;
+
 
   constructor() {
 
    }
 
   ngOnInit() {
+
+      this.viewState$ = this.data$.pipe(pluck('artistServiceState'));
   }
 
 
