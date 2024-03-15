e78959a493e062a2d876687e26dacd84f502861d
Richard Coll <richarddcoll@gmail.com>
2024-03-15 18:52:07 +0000

    adding fields to create music release tracks

--- src/app/createmusicrelease/createmusicrelease.component.ts
+++ src/app/createmusicrelease/createmusicrelease.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
 import { MusicReleaseInputData } from '../models/musicrelease.model';
-import { FormBuilder, Validators, FormGroup} from '@angular/forms';
+import { FormBuilder, Validators, FormGroup, FormArray} from '@angular/forms';
 import { MusicRelease } from '../models/musicrelease.model';
 
 @Component({
@@ -40,7 +40,23 @@ export class CreatemusicreleaseComponent implements OnInit {
    }
 
   ngOnInit() {
+
+       this.musicReleaseForm = this.formBuilder.group({
+            items: this.formBuilder.array([this.createItem()])
+          })
   }
+
+
+  createItem() {
+      return this.formBuilder.group({
+        track: ['addtrack'],
+        length: ['3:09']
+      })
+    }
+
+  addNext() {
+      (this.formBuilder.control['items'] as FormArray).push(this.createItem())
+    }
 
   isFieldInvalid(field: string) {
 
@@ -60,8 +76,8 @@ export class CreatemusicreleaseComponent implements OnInit {
   onSubmit(){
 
 
-        alert('saving');
-        alert(this.musicReleaseForm.value.title);
+        //alert('saving');
+        //alert(this.musicReleaseForm.value.title);
 
       }
 
