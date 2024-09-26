5f2de52d80e8d499866771a7a1a06cd442895faa
Richard Coll <richarddcoll@gmail.com>
2024-09-26 14:55:40 +0100

    add chat and openai func

--- src/app/shared/openaiapi.service.ts
+++ src/app/shared/openaiapi.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,13 @@ export class OpenaiapiService {
   constructor(private http: HttpClient) { }
 
     public sendMessage(message: string) {
-      return this.http.post<any>(`${this.apiUrl}`, { message });
+
+      const headers = new HttpHeaders({
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${environment.apiKey}`
+          });
+
+      return this.http.post<any>(`${this.apiUrl}`, { message }, { headers } );
+      //return this.http.post<any>(`${this.apiUrl}`, { model: 'gpt-4', messages: message }, { this.headers });
     }
 }
