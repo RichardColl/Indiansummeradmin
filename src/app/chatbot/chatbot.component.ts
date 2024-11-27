63ece961cba50ea6194257d143fc939701d58589
Richard Coll <richarddcoll@gmail.com>
2024-11-27 17:56:45 +0000

    take json from chat and save as musicrelease

--- src/app/chatbot/chatbot.component.ts
+++ src/app/chatbot/chatbot.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MusicRelease, Track } from '../models/musicrelease.model';
 
 import { OpenaiapiService } from '../shared/openaiapi.service';
+import { MusicreleaseService } from '../shared/musicrelease/musicrelease.service';
 
 @Component({
   selector: 'app-chatbot',
@@ -9,10 +11,18 @@ import { OpenaiapiService } from '../shared/openaiapi.service';
 })
 export class ChatbotComponent {
   userMessage!: string;
+  musicReleaseData!: string;
   assistantReply!: string;
   chatMessages: { role: string, content: string }[] = [];
+  genresarray: string[]= [];
 
-  constructor(private openAiApiService: OpenaiapiService){}
+  musicrelease = new MusicRelease();
+  tmptrack = new Track();
+
+  result:string;
+  trackarray: Track[]= [];
+
+  constructor(private openAiApiService: OpenaiapiService, private musicreleaseServ: MusicreleaseService){}
 
   sendMessage() {
     const userMessage = this.userMessage;
@@ -24,4 +34,57 @@ export class ChatbotComponent {
         this.userMessage = '';
       });
   }
+
+  saveMusicReleaseData() {
+
+              const obj = JSON.parse(this.musicReleaseData);
+
+              this.musicrelease.title = obj.title;
+              this.musicrelease.artistid = '12345';
+              this.musicrelease.artistname = obj.artist;
+              this.musicrelease.shortdescription = obj.description;
+              this.musicrelease.type = 'VINYL';
+              this.musicrelease.frontcoverimage = '12345';
+              this.musicrelease.display = false;
+
+              this.genresarray.push('INDIE');
+              this.musicrelease.genres = this.genresarray;
+
+              this.tmptrack.title = 'no tracks available for display';
+              this.tmptrack.trackno = '1';
+              this.tmptrack.length = '0:00';
+              this.trackarray.push(this.tmptrack);
+              this.musicrelease.tracks= this.trackarray;
+
+              this.musicrelease.releasedate = '27/11/24';
+              this.musicrelease.releaseaddeddate = '27/11/24';
+              this.musicrelease.vinylformat = '1 x LP black vinyl';
+              this.musicrelease.label = obj.label;
+              this.musicrelease.catalogueno = '1';
+              this.musicrelease.barcode = obj.barcode;
+              this.musicrelease.price = '1';
+              this.musicrelease.instock = '1';
+              this.musicrelease.releaseyear = obj.year;
+
+              this.musicrelease.pricepaidsterling= '1';
+              this.musicrelease.exchangerate= '1';
+              this.musicrelease.pricepaideuro= '1';
+              this.musicrelease.invoiceno= '1';
+              this.musicrelease.datepaid= '1';
+
+              this.musicreleaseServ.save(this.musicrelease)
+                                               .subscribe(
+                                                   data => {
+                                                   if (data) {
+                                                        this.result = "successful Create -- musicrelease ";
+                                                    }
+                                                    window.location.reload();
+                                                   },
+                                                   error => {
+                                                      this.result = "create musicrelease  failed";
+                                                   });
+
+
+    }
+
 }
