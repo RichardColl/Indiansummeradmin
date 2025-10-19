bb0c83f69b399e8be5db348351e84f9171275497
Richard Coll <richarddcoll@gmail.com>
2025-10-19 16:33:03 +0100

    add new artist container

--- src/app/musicreleasescontainer/musicreleasescontainer.component.ts
+++ src/app/musicreleasescontainer/musicreleasescontainer.component.ts
@@ -3,6 +3,20 @@ import { CreatemusicreleaseComponent } from '../createmusicrelease/createmusicre
 import { ISModalComponent, ModalService, AbstractModalService, ISModalModel } from '../shared/modals/modal.service';
 import { MusicReleaseInputData } from '../models/musicrelease.model';
 
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { ArtistService } from '../shared/artist/artist.service';
+import { AdminDispatcher, AdminType } from '../shared/utils/listener.service';
+import { Observable } from 'rxjs/Observable';
+import { map, tap } from 'rxjs/operators';
+import { ServiceState } from '../shared/main-api.service';
+import { ArtistData } from '../shared/artist.abstract.service';
+
+
+
+import { ArtistServiceData } from '../shared/artist.abstract.service';
+
+
 @Component({
   selector: 'musicreleasescontainer',
   templateUrl: './musicreleasescontainer.component.html',
@@ -10,10 +24,64 @@ import { MusicReleaseInputData } from '../models/musicrelease.model';
 })
 export class MusicreleasescontainerComponent implements OnInit {
 
-  constructor(private modalOutletService: AbstractModalService) { }
+        collection = { count: 6, data: [] };
+        thedata:any;
+        result = [];
+
+        ServiceStateEnum = ServiceState;
+
+  constructor(private modalOutletService: AbstractModalService,
+                private artistService: ArtistService,  private readonly adminDispatcher: AdminDispatcher) { }
+
+
+  displayArtistsOptionsState$:Observable<ServiceState>
+            = this.artistService.serviceData$.pipe(
+              map(({ artistServiceState, artistDetails }) => {
+
+              if(artistServiceState === this.ServiceStateEnum.SUCCESS) {
+                this.thedata = artistDetails;
+                this.collection.data = this.thedata._embedded.artists;
+                this.result.push(this.collection.data[this.randomInteger(10, 50)]);
+
+              }
+
+              return artistServiceState;
+              })
+            );
+
+          artistDetails: ArtistData;
+
+        readonly viewData$ = this.artistService.serviceData$.pipe(
+
+              map(response => {
+
+
+              } ),
+               tap(model => {
+
+                             this.adminDispatcher.dispatch(
+                                 AdminType.ARTIST_LIST,
+                                 model as ArtistServiceData
+                             );
+                })
+             ) as Observable<ArtistServiceData>;
 
   ngOnInit() {
+
+    this.artistService.getArtistsByDisplay();
+
   }
+
+
+
+   randomInteger(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+
+
+
+
 
   handleCreateMusicRelease() {
       alert('createe');
