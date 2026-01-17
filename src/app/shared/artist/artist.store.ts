05a40df385922a6cbdeaac58c749c1c615c78e94
Richard Coll <richarddcoll@gmail.com>
2026-01-17 15:31:17 +0000

    store managemnet impl

---
+++ src/app/shared/artist/artist.store.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { ArtistServiceData } from '../artist.abstract.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArtistStore {
+
+//    artists$ : Observable<ArtistData[]>;
+
+
+
+
+}
--- src/app/storeexample/storeexample.component.ts
+++ src/app/storeexample/storeexample.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-storeexample',
-  templateUrl: './storeexample.component.html',
-  styleUrls: ['./storeexample.component.css']
+import { TrackStore } from '../shared/track/track.store';
+
+import { Observable } from 'rxjs/Observable';
+import { map, tap } from 'rxjs/operators';
+import { ServiceState } from '../shared/main-api.service';
+import { TrackData } from '../shared/track.abstract.service';
+
+
+
+import { TrackServiceData } from '../shared/track.abstract.service';
+
 })
 export class StoreexampleComponent implements OnInit {
 
-  constructor() { }
+  tracks$: Observable<TrackData[]>;
+
+  constructor(private trackStore: TrackStore) {
+
+
+
+  }
 
   ngOnInit() {
+
+      this.reloadTracks();
+  }
+
+  reloadTracks() {
+
+      this.tracks$ = this.trackStore.getDefaultTrack();
+
   }
 
 }
